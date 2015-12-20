3d65d96b993e0cbbd24675b5efb41020dbd5a867
eabourflower <970250685@qq.com>
2015-12-20 17:45:29 +0800

    merge

---
+++ src/main/java/cc/eabour/websocket/AppMain.java
@@ -0,0 +1,10 @@
+package cc.eabour.websocket;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>WebSocket Demo</title>
+</head>
+<body>
+<h2>Hello World!</h2>
+<hr>
+</body>
+</html>
